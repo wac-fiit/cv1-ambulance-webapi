fad70de96f926ffb38bd186d8b540b6c6ac8b87e
Peter Micuch <peter.micuch@siemens-healthineers.com>
2026-03-29 06:30:52 +0000

    first commit

---
+++ cmd/ambulance-api-service/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"log"
+	"os"
+	"strings"
+
+	"context"
+	"time"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"github.com/wac-fiit/ambulance-webapi-cv1/api"
+	"github.com/wac-fiit/ambulance-webapi-cv1/internal/ambulance_wl"
+	"github.com/wac-fiit/ambulance-webapi-cv1/internal/db_service"
+)
+
+func main() {
+	log.Printf("Server started")
+	port := os.Getenv("AMBULANCE_API_PORT")
+	if port == "" {
+		port = "8080"
+	}
+	environment := os.Getenv("AMBULANCE_API_ENVIRONMENT")
+	if !strings.EqualFold(environment, "production") { // case insensitive comparison
+		gin.SetMode(gin.DebugMode)
+	}
+	engine := gin.New()
+	engine.Use(gin.Recovery())
+	corsMiddleware := cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "PUT", "POST", "DELETE", "PATCH"},
+		AllowHeaders:     []string{"Origin", "Authorization", "Content-Type"},
+		ExposeHeaders:    []string{""},
+		AllowCredentials: false,
+		MaxAge:           12 * time.Hour,
+	})
+	engine.Use(corsMiddleware)
+
+	// setup context update  middleware
+	dbService := db_service.NewMongoService[ambulance_wl.Ambulance](db_service.MongoServiceConfig{})
+	defer dbService.Disconnect(context.Background())
+	engine.Use(func(ctx *gin.Context) {
+		ctx.Set("db_service", dbService)
+		ctx.Next()
+	})
+	// request routings
+	handleFunctions := &ambulance_wl.ApiHandleFunctions{
+		AmbulanceConditionsAPI:  ambulance_wl.NewAmbulanceConditionsApi(),
+		AmbulanceWaitingListAPI: ambulance_wl.NewAmbulanceWaitingListApi(),
+		AmbulancesAPI:           ambulance_wl.NewAmbulancesApi(),
+	}
+	ambulance_wl.NewRouterWithGinEngine(engine, *handleFunctions)
+	engine.GET("/openapi", api.HandleOpenApi)
+	engine.Run(":" + port)
+}
