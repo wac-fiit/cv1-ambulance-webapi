5524675224b206d4a94ea43723769feff77bbc36
Peter Micuch <peter.micuch@siemens-healthineers.com>
2026-04-27 10:05:58 +0000

    distributed tracing

--- cmd/ambulance-api-service/main.go
+++ cmd/ambulance-api-service/main.go
@@ -14,6 +14,12 @@ import (
 	"github.com/wac-fiit/ambulance-webapi-cv1/api"
 	"github.com/wac-fiit/ambulance-webapi-cv1/internal/ambulance_wl"
 	"github.com/wac-fiit/ambulance-webapi-cv1/internal/db_service"
+
+	"go.opentelemetry.io/contrib/exporters/autoexport"
+	"go.opentelemetry.io/contrib/instrumentation/github.com/gin-gonic/gin/otelgin"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/propagation"
+	tracesdk "go.opentelemetry.io/otel/sdk/trace"
 )
 
 func main() {
@@ -33,6 +39,19 @@ func main() {
 	// Set the global log level
 	zerolog.SetGlobalLevel(level)
 
+	// initialize trace exporter
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	traceExporter, err := autoexport.NewSpanExporter(ctx)
+	if err != nil {
+		log.Fatal().Err(err).Msg("Failed to initialize trace exporter")
+	}
+
+	traceProvider := tracesdk.NewTracerProvider(tracesdk.WithBatcher(traceExporter))
+	otel.SetTracerProvider(traceProvider)
+	otel.SetTextMapPropagator(propagation.TraceContext{})
+	defer traceProvider.Shutdown(ctx)
+
 	log.Info().Msg("Server started")
 
 	port := os.Getenv("AMBULANCE_API_PORT")
@@ -45,6 +64,8 @@ func main() {
 	}
 	engine := gin.New()
 	engine.Use(gin.Recovery())
+	engine.Use(otelgin.Middleware("ambulance-webapi"))
+
 	corsMiddleware := cors.New(cors.Config{
 		AllowOrigins:     []string{"*"},
 		AllowMethods:     []string{"GET", "PUT", "POST", "DELETE", "PATCH"},
